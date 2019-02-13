5fa037192d765659fd2c1dc4751dd927adeeef90
Terry Phung <terry.phung@gmail.com>
2019-02-13 17:03:24 +0000

    version 0.0.4 - updated Shrimp (complete)

--- Carnivores.java
+++ Carnivores.java
@@ -7,18 +7,12 @@ import java.util.List;
  */
 public abstract class Carnivores extends Organism
 {
-    // instance variables - replace the example below with your own
-    private int x;
 
-    /**
-     * Constructor for objects of class Fish
-     */
     public Carnivores(Field field, Location location)
     {
         // initialise instance variables
         super(field, location);
     }
-
 
     public void act(List<Organism> newAnimals){}
 }
